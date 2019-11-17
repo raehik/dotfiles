727215c4f663c139a6dd65c9146a4b71957c0be6
Ben Orchard <thefirstmuffinman@gmail.com>
2019-11-17 21:23:40 +0000

    graphical: small changes all over to improve desktop BGs

--- scripts-general/bin/bg-cycle.py
+++ scripts-general/bin/bg-cycle.py
@@ -3,24 +3,119 @@
 # Create backgrounds for applications.
 #
 
-import raehutils
-import sys, os, argparse, logging
+import sys, os, argparse, logging, subprocess
+from pathlib import Path
 
-import random
-import glob
+import random, glob
 
-class BGCycler(raehutils.RaehBaseClass):
-    BACKGROUNDS = ["desktop", "grub"]
-    VALID_IMG_EXTS = ["png", "jpg"]
-    GRUB_BG_IMG = "/boot/grub/background-image.png"
+## Boilerplate utils class {{{
+class BoilerplateClass:
+    def _deinit(self):
+        self.logger.debug("deinitialising...")
 
-    DEF_BG_DIR = os.path.join(os.environ.get("HOME"), ".assets", "backgrounds")
-    DEF_WIDTH = "1366"
-    DEF_HEIGHT = "768"
+    def _init_logging(self):
+        self.logger = logging.getLogger(os.path.basename(sys.argv[0]))
+        lh = logging.StreamHandler()
+        lh.setFormatter(logging.Formatter("%(name)s: %(levelname)s: %(message)s"))
+        self.logger.addHandler(lh)
+
+    def _parse_verbosity(self):
+        if self.args.verbose == 1:
+            self.logger.setLevel(logging.INFO)
+        elif self.args.verbose >= 2:
+            self.logger.setLevel(logging.DEBUG)
+        if self.args.quiet >= 1:
+            # reset verbosity (to make verbose/quiet checks easier)
+            self.args.verbose = 0
+            self.logger.setLevel(logging.NOTSET)
+
+    def run(self):
+        """Run from CLI: parse arguments, run main, deinitialise."""
+        self._init_logging()
+        self._parse_args()
+        self.main()
+        self._deinit()
+
+    def fail(self, msg="internal error", ret=1):
+        """Exit with a message and a return code.
+
+        Should only be used for errors -- if you want to deinitialise and exit
+        safely, simply return from main.
+
+        Suggested to use no parameters for internal functions that the user
+        doesn't need to know about (which generally indicates a logic error,
+        rather than an input one.)
+        """
+        self.logger.error(msg)
+        self._deinit()
+        sys.exit(ret)
+
+    def get_shell(self, cmd, cwd=None):
+        """Run a shell command, blocking execution, detaching stdin, stdout and
+        stderr.
+
+        Useful for grabbing shell command outputs, or if you want to run something
+        silently and wait for it to finish.
+
+        @param cmd command to run as an array, where each element is an argument
+        @param cwd if present, directory to use as CWD
+        @return the command's return code, stdout and stderr (respectively, as a
+                tuple)
+        """
+        proc = subprocess.run(cmd, stdin=subprocess.DEVNULL,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE,
+                                   cwd=cwd)
+        return proc.returncode, \
+               proc.stdout.decode("utf-8", "replace").strip(), \
+               proc.stderr.decode("utf-8", "replace").strip()
+
+    def get_shell_with_input(self, cmd, stdin_in, cwd=None):
+        """Run a shell command with a given string passed to stdin, blocking
+        execution and detaching stdout and stderr.
+
+        We put a newline on the end of stdin_in, because that appears to be important for
+        some programs (e.g. bc). TODO though, unsure. Maybe should be an option.
+
+        @param cmd    command to run as an array, where each element is an argument
+        @param std_in string to pass to stdin
+        @param cwd    if present, directory to use as CWD
+        @return the command's return code, stdout and stderr (respectively, as a
+                tuple)
+        """
+        proc = subprocess.run(cmd, input=bytes("{}\n".format(stdin_in), "utf-8"),
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE,
+                                   cwd=cwd)
+        return proc.returncode, \
+               proc.stdout.decode("utf-8", "replace").strip(), \
+               proc.stderr.decode("utf-8", "replace").strip()
+
+    def drop_to_shell(self, cmd, cwd=None):
+        """Run a shell command, blocking execution.
+
+        Doesn't touch any pipes. Like dropping to shell during execution.
+
+        @param cmd command to run as an array, where each element is an argument
+        @param cwd if present, directory to use as CWD
+        @return the command's exit code
+        """
+        return subprocess.run(cmd, cwd=cwd).returncode
+    ## Boilerplate utils class }}}
+
+class BGCycler(BoilerplateClass):
+    DEF_IMG_DIR = Path(os.environ.get("HOME"), "proj", "media", "backgrounds")
+    DEF_OUT_DIR = Path(os.environ.get("XDG_DATA_HOME"), "backgrounds")
+
+    DEF_WIDTH = "1920"
+    DEF_HEIGHT = "1080"
     DEF_BRIGHTNESS = "-81"
     DEF_CONTRAST = "-91"
 
-    ERR_IMG_CONVERSION = 1
+    ERR_IMG_CONVERSION_FAILED = 2
+    ERR_IMG_FILE_ERROR = 3
+
+    DEF_BG_NAME = "desktop"
 
     ## CLI-related {{{
     def _parse_args(self):
@@ -29,9 +124,13 @@ class BGCycler(raehutils.RaehBaseClass):
                 epilog="I use a rule of contrast = brightness-10 for a nice image.")
         self.parser.add_argument("-v", "--verbose", help="be verbose", action="count", default=0)
         self.parser.add_argument("-q", "--quiet", help="be quiet (overrides -v)", action="count", default=0)
-        self.parser.add_argument("-d", "--bg-dir",
-                help="directory to read for images",
-                default=BGCycler.DEF_BG_DIR)
+        self.parser.add_argument("-d", "--img-dir",
+                help="directory to read for images")
+        self.parser.add_argument("-o", "--out-dir",
+                help="directory to place backgrounds in")
+        self.parser.add_argument("-n", "--bg-name",
+                help="background name (= filename in background directory)",
+                default=BGCycler.DEF_BG_NAME)
         self.parser.add_argument("-w", "--width",
                 help="width to resize images to (when required)",
                 default=BGCycler.DEF_WIDTH)
@@ -51,84 +150,47 @@ class BGCycler(raehutils.RaehBaseClass):
 
         self.args.verbose += 1 # force some verbosity
         self._parse_verbosity()
+
+        if self.args.img_dir is not None:
+            self.img_dir = Path(self.args.img_dir)
+        else:
+            self.img_dir = BGCycler.DEF_IMG_DIR
+
+        if self.args.out_dir is not None:
+            self.out_dir = Path(self.args.out_dir)
+        else:
+            self.out_dir = BGCycler.DEF_OUT_DIR
+
     ## }}}
 
-    def main(self):
-        """Main entrypoint after program setup."""
-        imgs = self.get_shuffled_images(self.args.bg_dir)
+    def create_background(self, bg_name, image, brightness, contrast, width, height):
+        self.logger.info("{} -> {}".format(bg_name, image))
 
-        if len(BGCycler.BACKGROUNDS) > len(imgs):
-            self.fail("not enough images for the backgrounds required", BGCycler.ERR_IMG_CONVERSION)
+        background_path = Path(self.out_dir, bg_name+".png")
 
-        for bg, img in zip(BGCycler.BACKGROUNDS, imgs):
-            # get outfile
-            img_converted = os.path.join(
-                self.args.bg_dir,
-                "converted",
-                os.path.basename(os.path.splitext(img)[0]) + ".png")
-
-            # convert
-            self.logger.info(os.path.basename(img))
-            if bg == "grub":
-                # special override: resize, copy to GRUB background image
-                self.convert_image(img, img_converted, self.args.brightness,
-                    self.args.contrast, self.args.width, self.args.height)
-                raehutils.drop_to_shell(["sudo", "cp", img_converted, BGCycler.GRUB_BG_IMG])
-            else:
-                self.convert_image(img, img_converted, self.args.brightness,
-                    self.args.contrast)
-
-            # make symlink
-            bg_out_symlink = os.path.join(self.args.bg_dir, bg)
-            try:
-                # remove symlink if it already exists
-                os.remove(bg_out_symlink)
-            except FileNotFoundError:
-                pass
-            os.symlink(
-                os.path.join("converted", os.path.basename(img_converted)),
-                bg_out_symlink)
-
-        # now set the background
-        raehutils.drop_to_shell([os.path.join(self.args.bg_dir, "set-desktop-background")])
-
-    def get_shuffled_images(self, d):
-        """Fills an array with images in a specified directory, shuffles it,
-        then prepends user-provided images."""
-        imgs = []
-
-        # get all backgrounds
-        for ext in BGCycler.VALID_IMG_EXTS:
-            imgs.extend(glob.glob(os.path.join(d, "original", "*." + ext)))
-
-        random.shuffle(imgs)
-
-        # now insert user images at the start of the list
-        if self.args.images:
-            # reverse since we're inserting at index 0 every time
-            for img in reversed(self.args.images):
-                if img == "":
-                    # not giving file for this background: skip
-                    continue
-                else:
-                    # assume images are provided relative to the given dir
-                    imgs.insert(0, os.path.join(d, "original", img))
-        return imgs
-
-    def convert_image(self, img, img_converted, brightness, contrast, width="0", height="0"):
-        cmd_convert_img = [
-                "convert", img,
-                "-brightness-contrast", "{}x{}".format(brightness, contrast)]
+        cmd_convert_img = ["convert", image, "-brightness-contrast", "{}x{}".format(brightness, contrast)]
         if width == "0" or height == "0":
             pass
         else:
             cmd_convert_img += [
                     "-crop", "{}x{}+0+0".format(width, height),
                     "-gravity", "center"]
-        cmd_convert_img.append(img_converted)
-        ret = raehutils.drop_to_shell(cmd_convert_img)
+        cmd_convert_img.append(background_path)
+        ret = self.drop_to_shell(cmd_convert_img)
         if ret != 0:
-            self.fail("couldn't convert image {}".format(img), BGCycler.ERR_IMG_CONVERSION)
+            self.fail("failed converting for background {}, image: {}".format(background, image), BGCycler.ERR_IMG_CONVERSION_FAILED)
+
+    def main(self):
+        """Main entrypoint after program setup."""
+        imgs = [x for x in self.img_dir.glob("**/*") if x.is_file()]
+        random.shuffle(imgs)
+        for img in self.args.images:
+            imgs.append(img)
+
+        if len(imgs) == 0:
+            self.fail("no images available", BGCycler.ERR_IMG_FILE_ERROR)
+
+        self.create_background(self.args.bg_name, imgs.pop(), self.args.brightness, self.args.contrast, self.args.width, self.args.height)
 
 if __name__ == "__main__":
     program = BGCycler()
